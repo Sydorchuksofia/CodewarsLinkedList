3883dc57f01a39a284f415f7f01ba283ecfe6f47
Sydorchuk Sofia <sydorchuk.pn@ucu.edu.ua>
2024-04-03 19:12:20 +0300

    implemented Recursive Reverse

---
+++ recursive_reverse.py
@@ -0,0 +1,13 @@
+class Node(object):
+    """Node class for reference"""
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+def reverse(head):
+    # your code goes here.
+    if head is None or head.next is None:
+        return head
+    rest = reverse(head.next)
+    head.next.next = head
+    head.next = None
+    return rest
